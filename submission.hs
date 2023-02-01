e165f76853baab7e96b8566cf5396780b33b8cfe
022by <46434255+pplulee@users.noreply.github.com>
2023-02-01 00:10:04 +0000

    update task 1

--- submission.hs
+++ submission.hs
@@ -1,17 +1,13 @@
 data Colour = Black | White deriving (Eq, Show)
-data Direction = Clockwise | Anticlockwise deriving (Eq, Show)
 data Quadtree = Leaf Colour | Node Quadtree Quadtree Quadtree Quadtree deriving (Eq, Show)
 
 allBlack :: Int -> Quadtree
-allBlack 0 = Leaf Black
-allBlack n = Node (allBlack (n-1)) (allBlack (n-1)) (allBlack (n-1)) (allBlack (n-1))
+allBlack _ = Leaf Black
 allWhite :: Int -> Quadtree
-allWhite 0 = Leaf White
-allWhite n = Node (allWhite (n-1)) (allWhite (n-1)) (allWhite (n-1)) (allWhite (n-1))
+allWhite _ = Leaf White
 
 clockwise :: Quadtree -> Quadtree -> Quadtree -> Quadtree -> Quadtree
-clockwise (Leaf a) (Leaf b) (Leaf c) (Leaf d) = Node (Leaf d) (Leaf a) (Leaf b) (Leaf c)
-
+clockwise a b c d = Node a b d c
 
 anticlockwise :: Quadtree -> Quadtree -> Quadtree -> Quadtree -> Quadtree
-anticlockwise (Leaf a) (Leaf b) (Leaf c) (Leaf d) = Node (Leaf b) (Leaf c) (Leaf d) (Leaf a)
+anticlockwise a b c d = Node a d b c
