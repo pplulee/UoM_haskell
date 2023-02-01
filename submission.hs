fd7d6f6d15a24bd95c40d3375dd7bfb2205d6241
022by <46434255+pplulee@users.noreply.github.com>
2023-02-01 20:34:11 +0000

    update

--- submission.hs
+++ submission.hs
@@ -11,3 +11,19 @@ clockwise a b c d = Node a b d c
 
 anticlockwise :: Quadtree -> Quadtree -> Quadtree -> Quadtree -> Quadtree
 anticlockwise a b c d = Node a d b c
+
+reverse_colour :: Colour -> Colour
+reverse_colour Black = White
+reverse_colour White = Black
+
+data Direction = NE | NW | SE | SW deriving (Eq, Show)
+
+-- a b
+-- c d
+striped :: Quadtree -> Bool
+striped (Leaf a) = True
+striped (Node a b c d) = a==b && c==d && striped a && striped c
+
+blur :: Quadtree -> Quadtree
+blur (Leaf a) = Leaf a
+blur (Node a b c d) = if striped (Node a b c d) then (Node a b c d) else (Node (blur a) (blur b) (blur c) (blur d))
